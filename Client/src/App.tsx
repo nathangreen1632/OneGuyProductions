b8ba65789858ea478286ff7cf82f94f50fcc0ec1
Nathan Green <ngreen1632@gmail.com>
2025-07-31 15:19:15 -0500

    `refactor(client): specify React.ReactElement return type for App component`

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import AppRoutes from './AppRoutes';
 
-export default function App() {
+export default function App(): React.ReactElement {
   return (
     <BrowserRouter>
       <div className="flex flex-col min-h-screen bg-[var(--theme-bg)] text-[var(--theme-text)]">
