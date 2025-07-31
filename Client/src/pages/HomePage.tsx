87693ff22fc98178c281c5711268f33994e5d5d3
Nathan Green <ngreen1632@gmail.com>
2025-07-31 15:22:01 -0500

    `refactor(client): specify React.ReactElement return type for HomePage component`

--- Client/src/pages/HomePage.tsx
+++ Client/src/pages/HomePage.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import HeroSection from '../components/HeroSection';
 import Philosophy from '../components/Philosophy.tsx';
 
-export default function HomePage() {
+export default function HomePage(): React.ReactElement {
   return (
     <div className="bg-[var(--theme-bg)] text-[var(--theme-text)] min-h-screen flex flex-col">
       <main className="flex-grow">
