15051c8cd1f4db883bf04a2d153c7bd08f745f44
Nathan Green <ngreen1632@gmail.com>
2025-07-31 15:18:57 -0500

    `refactor(client): specify React.ReactElement return type for AboutPage component`

--- Client/src/pages/AboutPage.tsx
+++ Client/src/pages/AboutPage.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import AboutSection from '../components/AboutSection';
 
-export default function AboutPage() {
+export default function AboutPage(): React.ReactElement {
   return (
     <div className="bg-[var(--theme-bg)] min-h-screen text-[var(--theme-text)]">
       <AboutSection />
