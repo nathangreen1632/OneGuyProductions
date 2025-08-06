9ccc00e335939e5ee1b3b5ad9d9401d78580249e
Nathan Green <ngreen1632@gmail.com>
2025-08-06 08:42:31 -0500

    feat(server): add migration for users table creation
    - Introduced migration script to create `users` table.
    - Defined schema with fields: `id`, `username`, `email`, `password`, `createdAt`, and `updatedAt`.
    - Included `up` method for table creation and `down` method for table removal.

---
+++ Server/src/migrations/20250806060100-create-users-table.cjs
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('users', {
+            id: {
+                type: Sequelize.INTEGER.UNSIGNED,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false,
+            },
+            username: {
+                type: Sequelize.STRING(40),
+                allowNull: false,
+                unique: true,
+            },
+            email: {
+                type: Sequelize.STRING(120),
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+        });
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('users');
+    },
+};
