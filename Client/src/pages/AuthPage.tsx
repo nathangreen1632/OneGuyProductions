564d7f6014aaaabf2fc01036ebb6e46253cdde30
Nathan Green <ngreen1632@gmail.com>
2025-08-07 23:01:25 -0500

    refactor(client): update AuthPage and AuthForm import paths
    - Renamed `Auth.tsx` to `AuthPage.tsx` for improved clarity and alignment with naming conventions.
    - Updated `AuthForm` import path to reflect its renaming to `AuthFormLogic.tsx`.

--- Client/src/pages/AuthPage.tsx
+++ Client/src/pages/AuthPage.tsx
@@ -1,6 +1,6 @@
 // Client/src/pages/Auth.tsx
 import React from 'react';
-import AuthForm from '../components/AuthForm';
+import AuthForm from '../components/AuthFormLogic.tsx';
 import ResetPasswordModal from '../modals/ResetPasswordModal.tsx';
 
 export default function AuthPage(): React.ReactElement {
