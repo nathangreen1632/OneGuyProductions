47b1ee17c1f85c52f475bc15b476da1c96580ada
Nathan Green <ngreen1632@gmail.com>
2025-08-07 22:41:10 -0500

    refactor(client): update ResetPasswordModal import path in AuthPage
    - Adjusted the import path for `ResetPasswordModal` to reflect its relocation to the `modals` directory.
    - Improved project structure by organizing modal components in a dedicated folder.

--- Client/src/pages/Auth.tsx
+++ Client/src/pages/Auth.tsx
@@ -1,7 +1,7 @@
 // Client/src/pages/Auth.tsx
 import React from 'react';
 import AuthForm from '../components/AuthForm';
-import ResetPasswordModal from '../components/ResetPasswordModal';
+import ResetPasswordModal from '../modals/ResetPasswordModal.tsx';
 
 export default function AuthPage(): React.ReactElement {
   return (
