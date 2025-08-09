d6e3737d7f7a6dd7fbe1d657f92cfc489e452124
Nathan Green <ngreen1632@gmail.com>
2025-08-09 17:34:16 -0500

    feat(server): add admin route to index and reorder imports/routes
    - Introduced `admin.route` to the router for `'/admin'` endpoint handling.
    - Reorganized imports and route definitions for better readability and logical grouping.

--- Server/src/routes/index.ts
+++ Server/src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
-import orderRoutes from './order.route.js';
-import contactRoutes from './contact.route.js';
+import adminRoutes from './admin.route.js';
 import authRoutes from './auth.route.js';
+import contactRoutes from './contact.route.js';
+import orderRoutes from './order.route.js';
 
 const router: Router = Router();
 
-router.use('/order', orderRoutes);
-router.use('/contact', contactRoutes);
+router.use('/admin', adminRoutes);
 router.use('/auth', authRoutes);
+router.use('/contact', contactRoutes);
+router.use('/order', orderRoutes);
 
 export default router;
