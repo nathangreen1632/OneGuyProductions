c8d99c1fd30bfe936d8a1ee5d8e6f5eb0e4fb9a2
Nathan Green <ngreen1632@gmail.com>
2025-08-11 21:08:22 -0500

    feat(server): add database configuration for environments
    - Created `config.cjs` to define database configurations for development and production environments.
    - Enabled environment variable-based configuration (`DATABASE_URL`) to simplify deployment.
    - Added SSL support for production with Sequelize options for secure connections.
    - Introduced custom migration and seeding storage settings for better management.

---
+++ Server/config/config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    development: {
+        use_env_variable: 'DATABASE_URL',
+        dialect: 'postgres',
+        logging: false
+    },
+    production: {
+        use_env_variable: 'DATABASE_URL',
+        dialect: 'postgres',
+        logging: false,
+        dialectOptions: {
+
+            ssl: { require: true, rejectUnauthorized: false }
+        },
+        migrationStorageTableName: 'SequelizeMeta',
+        seederStorage: 'sequelize'
+    }
+};
